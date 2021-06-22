120c2b70cef776bad1b002caf32581b6964f266c
rjlucasm <rjlucasmonteiro@gmail.com>
2021-06-22 13:57:05 -0300

    today

---
+++ components/footer.js
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        Powered by
+          <span>
+            <a href="https://dfinity.org/" target="_blank" rel="noopener noreferrer">
+              <Image src="/dfinity.svg" alt="Dfinity Logo" width={33} height={16} />
+            </a>
+          </span> 
+        &amp;
+          <span>
+            <a href="https://fleek.co/" target="_blank" rel="noopener noreferrer">
+                <Image src="/fleek.svg" alt="Fleek Logo" width={23} height={32} />
+            </a>
+          </span> 
+        |
+        <a href="https://www.coingecko.com/en/api" target="_blank" rel="noopener noreferrer">Crypto API Data Provided By Coingecko</a>
+
+      </footer>
+);
+  
+export default Footer;
