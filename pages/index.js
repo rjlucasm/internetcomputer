4f39628c05368c935a84b7b0d34ff229384386f1
rjlucasm <rjlucasmonteiro@gmail.com>
2021-06-23 18:39:22 -0300

    fixResponsive

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Header from '../components/header'
-import Footer from '../components/footer'
 import Image from 'next/image'
-import logo from '../public/icp.png'
 import styles from '../styles/Home.module.css'
+import logo from '../public/icp.png'
 import useSWR, { SWRConfig } from 'swr'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
@@ -12,25 +10,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Internet Computer Today</title>
+      <title>Internet Computer Today</title>
         <meta name="description" content="Internet Computer Today" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <Header /> */}
-
       <main className={styles.main}>
-        <h2 className={styles.title}>Internet Computer Today</h2>
-        
+        <h2 className={styles.title}>
+          Internet Computer Today
+        </h2>
+
         <Image src={logo} alt="icp" width={119} height={58} />
-        
+
         <><SWRConfig value = {{ fetcher }}>
           <Icp />
         </SWRConfig></>
       </main>
-
-      <Footer/>
-      
     </div>
   )
 }
@@ -45,16 +40,19 @@ function Icp(){
 
   return (
     <div className={styles.grid}>
-      <div className={styles.datas}>
-        <div>Price <p>${data["internet-computer"].usd}</p></div>
+      <div className={styles.card}>
+        <h4>Price</h4> 
+        <p>${data["internet-computer"].usd}</p>
       </div>
       
-      <div className={styles.datas}>
-        <div>Market Cap <p>${data["internet-computer"].usd_market_cap.toLocaleString('en-US', {maximumFractionDigits:0})}</p></div>
+      <div className={styles.card}>
+        <h4>Market Cap</h4> 
+        <p>${data["internet-computer"].usd_market_cap.toLocaleString('en-US', {maximumFractionDigits:0})}</p>
       </div>
        
-      <div className={styles.datas}>
-        <div>24h Volume <p>${data["internet-computer"].usd_24h_vol.toLocaleString('en-US', {maximumFractionDigits:0})}</p></div>
+      <div className={styles.card}>
+        <h4>24h Volume</h4> 
+        <p>${data["internet-computer"].usd_24h_vol.toLocaleString('en-US', {maximumFractionDigits:0})}</p>
       </div>
     </div>
   )
