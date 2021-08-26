24ce1fdd34c675a5dfbe6eb8dd5daabeb789c29e
rjlucasm <rjlucasmonteiro@gmail.com>
2021-08-26 18:55:11 -0300

    watchlist

---
+++ pages/watchlist.js
@@ -0,0 +1,82 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/footer'
+
+export default function Watchlist() {
+    return (
+    <div className={styles.container}>
+        <Head>
+        <title>Watchlist</title>
+        </Head>
+        
+        <main className={styles.main}>
+            <h2 className={styles.title}>Watchlist</h2>
+
+            <>&#128064;</>
+
+            <Link href="/"><a>Home</a></Link> 
+
+            <div className={styles.gamegrid}>
+            <div className={styles.gcard}>
+                <a href="https://eemeo-taaaa-aaaad-qakjq-cai.ic.fleek.co/" target="_blank" rel="noopener noreferrer">Aedile</a> - Project management dapp
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://aizxe-qaaaa-aaaah-aapya-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">AstroX</a> - Dart developer tools and “mini apps” framework
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://bv7wf-6qaaa-aaaai-qahbq-cai.raw.ic0.app/sign-up" target="_blank" rel="noopener noreferrer">CanCan</a> - A scalable video sharing service
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://www.civol.co/" target="_blank" rel="noopener noreferrer">Civol</a> - Asynchronous video conversation platform enabling decentralized discourse, debate, and collective decision-making
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://grwwk-gaaaa-aaaah-aapma-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">Content Fly</a> - Content marketplace connecting high-quality creators with brands
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://crowdrecords.com/" target="_blank" rel="noopener noreferrer">Crowd Records</a> - Decentralized economy for creating music
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://dab.ooo/" target="_blank" rel="noopener noreferrer">DAB</a> - An open internet service for interface-agnostic user profiles and data, token and NFT registries and verification, and more!
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://deckdeckgo.com/en/" target="_blank" rel="noopener noreferrer">DeckDeckGo</a> - Presentations on the Internet Computer
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://deland.one/" target="_blank" rel="noopener noreferrer">Deland</a> - Fungible Token Standard and DeFi components
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://dfinance.ai/" target="_blank" rel="noopener noreferrer">DFinance</a> - Foundational DeFi components for the Internet Computer
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://rglue-kyaaa-aaaah-qakca-cai.ic0.app/" target="_blank" rel="noopener noreferrer">IC Drive</a> - Decentralized file storage and sharing
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://sygsn-caaaa-aaaaf-qaahq-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">ICME</a> - A no-code website builder
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://icpunks.com/" target="_blank" rel="noopener noreferrer">ICPunks</a> - CryptoPunks clone on the Internet Computer
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://modclub.app/" target="_blank" rel="noopener noreferrer">ModClub</a> - Decentralized user generated content moderation service, to integrate with existing Internet Computer dapps
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://muga.media/nft" target="_blank" rel="noopener noreferrer">MugaTunes</a> - Music curation and NFT platform
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://kqre2-2qaaa-aaaad-qamxa-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">NFT Studio</a> - Mint, buy and sell 3DWASM NFTs
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://fxnaj-yaaaa-aaaaf-qad3q-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">Nuance</a> - Decentralized blogging platform
+            </div>
+            <div className={styles.gcard}>
+                <a href="https://voicemarkr.com/shell/map" target="_blank" rel="noopener noreferrer">VoiceMarkr</a> - P2P voice on the Internet Computer
+            </div> 
+            </div>
+        </main>
+
+        <Footer/>
+        
+    </div>
+    )
+}
