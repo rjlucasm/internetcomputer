e287deeeba1b89d82e1c4272ad5c243ad0743883
rjlucasm <rjlucasmonteiro@gmail.com>
2021-09-18 11:37:21 -0300

    dab

--- pages/services.js
+++ pages/services.js
@@ -28,6 +28,9 @@ export default function Services() {
                 <a href="https://faucet.dfinity.org/auth" target="_blank" rel="noopener noreferrer">Cycles Faucet</a> - Get free cycles to power your dapp
             </div>
             <div className={styles.gcard}>
+                <a href="https://dab.ooo/" target="_blank" rel="noopener noreferrer">DAB</a> - An open internet service for NFT, Token, Canister, and Dapp registries
+            </div>
+            <div className={styles.gcard}>
                 <a href="https://dank.ooo/" target="_blank" rel="noopener noreferrer">Dank</a> - Dank is a collection of Open Internet Services on the Internet Computer
             </div>
             <div className={styles.gcard}>
